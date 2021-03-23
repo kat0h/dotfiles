e8691a2759e9741483fe03f93ad592cccc79719f
KotaKato <peony.btn@gmail.com>
2021-03-23 23:40:57 +0900

    Update

--- emacs.d/custom.el
+++ emacs.d/custom.el
@@ -3,8 +3,47 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(auto-revert-interval 1)
+ '(auto-save-file-name-transforms '((".*" "~/.emacs.d/backup/" t)))
+ '(auto-save-interval 60)
+ '(auto-save-timeout 15)
+ '(backup-directory-alist '((".*" . "~/.emacs.d/backup") ("\\`/[^/:]+:[^/:]*:")))
+ '(counsel-find-file-ignore-regexp "\\(?:\\.\\(?:\\.?/\\)\\)")
+ '(counsel-yank-pop-separator "
+----------
+")
+ '(create-lockfiles nil)
+ '(custom-file "~/.emacs.d/custom.el")
+ '(debug-on-error t)
+ '(delete-old-versions t)
+ '(enable-recursive-minibuffers t)
+ '(frame-resize-pixelwise t)
+ '(history-delete-duplicates t)
+ '(history-length 1000)
+ '(indent-tabs-mode nil)
+ '(init-file-debug t t)
+ '(ivy-initial-inputs-alist nil)
+ '(ivy-prescient-retain-classic-highlighting t)
+ '(ivy-use-selectable-prompt t)
+ '(mouse-wheel-scroll-amount '(1 ((control) . 5)))
+ '(package-archives
+   '(("gnu" . "https://elpa.gnu.org/packages/")
+     ("melpa" . "https://melpa.org/packages/")
+     ("org" . "https://orgmode.org/elpa/")))
  '(package-selected-packages
-   '(atom-one-dark leaf-keywords ivy-prescient hydra el-get counsel blackout atom-one-dark-theme)))
+   '(neotree nyan-mode leaf-tree leaf-convert ivy-prescient hydra el-get doom-themes counsel blackout atom-one-dark-theme))
+ '(prescient-aggressive-file-save t)
+ '(ring-bell-function 'ignore)
+ '(scroll-bar-mode nil)
+ '(scroll-conservatively 100)
+ '(scroll-preserve-screen-position t)
+ '(show-paren-delay 0.1)
+ '(text-quoting-style 'straight)
+ '(truncate-lines t)
+ '(user-full-name "Kota Kato")
+ '(user-login-name "katokota" t)
+ '(user-mail-address "peony.btn@gmail.com")
+ '(version-control t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
--- emacs.d/init.el
+++ emacs.d/init.el
@@ -2,129 +2,129 @@
   (when (or load-file-name byte-compile-current-file)
     (setq user-emacs-directory
           (expand-file-name
-           (file-name-directory (or load-file-name byte-compile-current-file))))))
+            (file-name-directory (or load-file-name byte-compile-current-file))))))
 
 (eval-and-compile
   (customize-set-variable
-   'package-archives '(("gnu"   . "https://elpa.gnu.org/packages/")
-                       ("melpa" . "https://melpa.org/packages/")
-                       ("org"   . "https://orgmode.org/elpa/")))
+    'package-archives '(("gnu"   . "https://elpa.gnu.org/packages/")
+                        ("melpa" . "https://melpa.org/packages/")
+                        ("org"   . "https://orgmode.org/elpa/")))
   (package-initialize)
   (unless (package-installed-p 'leaf)
     (package-refresh-contents)
     (package-install 'leaf))
 
   (leaf leaf-keywords
-    :ensure t
-    :init
-    ;; optional packages if you want to use :hydra, :el-get, :blackout,,,
-    (leaf hydra :ensure t)
-    (leaf el-get :ensure t)
-    (leaf blackout :ensure t)
-
-    :config
-    ;; initialize leaf-keywords.el
-    (leaf-keywords-init)))
-
-    ;; leaf.elの設定
-    (leaf cus-edit
-        :doc "tools for customizing Emacs and Lisp packages"
-        :tag "builtin" "faces" "help"
-        :custom `((custom-file . ,(locate-user-emacs-file "custom.el"))))
-    (leaf cus-start
-        :doc "define customization properties of builtins"
-        :tag "builtin" "internal"
-        :preface
-        (defun c/redraw-frame nil
-            (interactive)
-            (redraw-frame))
-
-        :bind (("M-ESC ESC" . c/redraw-frame))
-        :custom '((user-full-name . "Kota Kato")
-                  (user-mail-address . "peony.btn@gmail.com")
-                  (user-login-name . "katokota")
-                  (create-lockfiles . nil)
-                  (debug-on-error . t)
-                  (init-file-debug . t)
-                  (frame-resize-pixelwise . t)
-                  (enable-recursive-minibuffers . t)
-                  (history-length . 1000)
-                  (history-delete-duplicates . t)
-                  (scroll-preserve-screen-position . t)
-                  (scroll-conservatively . 100)
-                  (mouse-wheel-scroll-amount . '(1 ((control) . 5)))
-                  (ring-bell-function . 'ignore)
-                  (text-quoting-style . 'straight)
-                  (truncate-lines . t)
-                  ;; (use-dialog-box . nil)
-                  ;; (use-file-dialog . nil)
-                  ;; (menu-bar-mode . t)
-                  ;; (tool-bar-mode . nil)
-                  (scroll-bar-mode . nil)
-                  (indent-tabs-mode . nil))
-        :config
-        (defalias 'yes-or-no-p 'y-or-n-p)
-        (keyboard-translate ?\C-h ?\C-?))
-
-        ;; 自動でファイルを読み直します
-        (leaf autorevert
-            :doc "revert buffers when files on disk change"
-            :tag "builtin"
-            :custom ((auto-revert-interval . 1))
-            :global-minor-mode global-auto-revert-mode)
-
-        ;; C系のファイルに関するモード
-        (leaf cc-mode
-            :doc "major mode for editing C and similar languages"
-            :tag "builtin"
-            :defvar (c-basic-offset)
-            :bind (c-mode-base-map
-                    ("C-c c" . compile))
-            :mode-hook
-            (c-mode-hook . ((c-set-style "bsd")
-                            (setq c-basic-offset 4)))
-            (c++-mode-hook . ((c-set-style "bsd")
-                                (setq c-basic-offset 4))))
-
-        ;; regionを上書きして挿入するマイナーモード
-        (leaf delsel
-            :doc "delete selection if you insert"
-            :tag "builtin"
-            :global-minor-mode delete-selection-mode)
-
-        ;; 対応する括弧を強調するマイナーモード
-        (leaf paren
-            :doc "highlight matching paren"
-            :tag "builtin"
-            :custom ((show-paren-delay . 0.1))
-            :global-minor-mode show-paren-mode)
-
-        ;; kill-ringの数を制御したり、kill-lineの挙動を変更したりする
-        (leaf files
-        :doc "file input and output commands for Emacs"
-        :tag "builtin"
-        :custom `((auto-save-timeout . 15)
-                    (auto-save-interval . 60)
-                    (auto-save-file-name-transforms . '((".*" ,(locate-user-emacs-file "backup/") t)))
-                    (backup-directory-alist . '((".*" . ,(locate-user-emacs-file "backup"))
-                                                (,tramp-file-name-regexp . nil)))
-                    (version-control . t)
-                    (delete-old-versions . t)))
-
-        (leaf ivy
-        :doc "Incremental Vertical completYon"
-        :req "emacs-24.5"
-        :tag "matching" "emacs>=24.5"
-        :url "https://github.com/abo-abo/swiper"
-        :emacs>= 24.5
         :ensure t
-        :blackout t
-        :leaf-defer nil
-        :custom ((ivy-initial-inputs-alist . nil)
-                (ivy-use-selectable-prompt . t))
-        :global-minor-mode t
-        :config
-        (leaf swiper
+        :init
+        ;; optional packages if you want to use :hydra, :el-get, :blackout,,,
+       (leaf hydra :ensure t)
+        (leaf el-get :ensure t)
+        (leaf blackout :ensure t)
+
+       :config
+        ;; initialize leaf-keywords.el
+        (leaf-keywords-init)))
+
+;; leaf.elの設定
+(leaf cus-edit
+      :doc "tools for customizing Emacs and Lisp packages"
+      :tag "builtin" "faces" "help"
+      :custom `((custom-file . ,(locate-user-emacs-file "custom.el"))))
+(leaf cus-start
+      :doc "define customization properties of builtins"
+      :tag "builtin" "internal"
+      :preface
+      (defun c/redraw-frame nil
+        (interactive)
+        (redraw-frame))
+
+      :bind (("M-ESC ESC" . c/redraw-frame))
+      :custom '((user-full-name . "Kota Kato")
+                (user-mail-address . "peony.btn@gmail.com")
+                (user-login-name . "katokota")
+                (create-lockfiles . nil)
+                (debug-on-error . t)
+                (init-file-debug . t)
+                (frame-resize-pixelwise . t)
+                (enable-recursive-minibuffers . t)
+                (history-length . 1000)
+                (history-delete-duplicates . t)
+                (scroll-preserve-screen-position . t)
+                (scroll-conservatively . 100)
+                (mouse-wheel-scroll-amount . '(1 ((control) . 5)))
+                (ring-bell-function . 'ignore)
+                (text-quoting-style . 'straight)
+                (truncate-lines . t)
+                ;; (use-dialog-box . nil)
+                ;; (use-file-dialog . nil)
+                ;; (menu-bar-mode . t)
+                ;; (tool-bar-mode . nil)
+                (scroll-bar-mode . nil)
+                (indent-tabs-mode . nil))
+      :config
+      (defalias 'yes-or-no-p 'y-or-n-p)
+      (keyboard-translate ?\C-h ?\C-?))
+
+;; 自動でファイルを読み直します
+(leaf autorevert
+      :doc "revert buffers when files on disk change"
+     :tag "builtin"
+      :custom ((auto-revert-interval . 1))
+      :global-minor-mode global-auto-revert-mode)
+
+;; C系のファイルに関するモード
+(leaf cc-mode
+      :doc "major mode for editing C and similar languages"
+      :tag "builtin"
+      :defvar (c-basic-offset)
+      :bind (c-mode-base-map
+              ("C-c c" . compile))
+      :mode-hook
+      (c-mode-hook . ((c-set-style "bsd")
+                      (setq c-basic-offset 4)))
+      (c++-mode-hook . ((c-set-style "bsd")
+                        (setq c-basic-offset 4))))
+
+;; regionを上書きして挿入するマイナーモード
+(leaf delsel
+      :doc "delete selection if you insert"
+      :tag "builtin"
+      :global-minor-mode delete-selection-mode)
+
+;; 対応する括弧を強調するマイナーモード
+(leaf paren
+      :doc "highlight matching paren"
+      :tag "builtin"
+      :custom ((show-paren-delay . 0.1))
+      :global-minor-mode show-paren-mode)
+
+;; kill-ringの数を制御したり、kill-lineの挙動を変更したりする
+(leaf files
+      :doc "file input and output commands for Emacs"
+      :tag "builtin"
+      :custom `((auto-save-timeout . 15)
+                (auto-save-interval . 60)
+                (auto-save-file-name-transforms . '((".*" ,(locate-user-emacs-file "backup/") t)))
+                (backup-directory-alist . '((".*" . ,(locate-user-emacs-file "backup"))
+                                            (,tramp-file-name-regexp . nil)))
+                (version-control . t)
+                (delete-old-versions . t)))
+
+(leaf ivy
+      :doc "Incremental Vertical completYon"
+      :req "emacs-24.5"
+      :tag "matching" "emacs>=24.5"
+      :url "https://github.com/abo-abo/swiper"
+      :emacs>= 24.5
+      :ensure t
+      :blackout t
+      :leaf-defer nil
+      :custom ((ivy-initial-inputs-alist . nil)
+               (ivy-use-selectable-prompt . t))
+      :global-minor-mode t
+      :config
+      (leaf swiper
             :doc "Isearch with an overview. Oh, man!"
             :req "emacs-24.5" "ivy-0.13.0"
             :tag "matching" "emacs>=24.5"
@@ -133,7 +133,7 @@
             :ensure t
             :bind (("C-s" . swiper)))
 
-        (leaf counsel
+      (leaf counsel
             :doc "Various completion functions using Ivy"
             :req "emacs-24.5" "swiper-0.13.0"
             :tag "tools" "matching" "convenience" "emacs>=24.5"
@@ -142,36 +142,49 @@
             :ensure t
             :blackout t
             :bind (("C-S-s" . counsel-imenu)
-                ("C-x C-r" . counsel-recentf))
+                   ("C-x C-r" . counsel-recentf))
             :custom `((counsel-yank-pop-separator . "\n----------\n")
-                    (counsel-find-file-ignore-regexp . ,(rx-to-string '(or "./" "../") 'no-group)))
+                      (counsel-find-file-ignore-regexp . ,(rx-to-string '(or "./" "../") 'no-group)))
             :global-minor-mode t))
 
-        (leaf prescient
-        :doc "Better sorting and filtering"
-        :req "emacs-25.1"
-        :tag "extensions" "emacs>=25.1"
-        :url "https://github.com/raxod502/prescient.el"
-        :emacs>= 25.1
-        :ensure t
-        :custom ((prescient-aggressive-file-save . t))
-        :global-minor-mode prescient-persist-mode)
+(leaf prescient
+      :doc "Better sorting and filtering"
+      :req "emacs-25.1"
+      :tag "extensions" "emacs>=25.1"
+      :url "https://github.com/raxod502/prescient.el"
+      :emacs>= 25.1
+      :ensure t
+      :custom ((prescient-aggressive-file-save . t))
+      :global-minor-mode prescient-persist-mode)
 
-        (leaf ivy-prescient
-        :doc "prescient.el + Ivy"
-        :req "emacs-25.1" "prescient-4.0" "ivy-0.11.0"
-        :tag "extensions" "emacs>=25.1"
-        :url "https://github.com/raxod502/prescient.el"
-        :emacs>= 25.1
-        :ensure t
-        :after prescient ivy
-        :custom ((ivy-prescient-retain-classic-highlighting . t))
-        :global-minor-mode t)
+(leaf ivy-prescient
+      :doc "prescient.el + Ivy"
+      :req "emacs-25.1" "prescient-4.0" "ivy-0.11.0"
+      :tag "extensions" "emacs>=25.1"
+      :url "https://github.com/raxod502/prescient.el"
+      :emacs>= 25.1
+      :ensure t
+      :after prescient ivy
+      :custom ((ivy-prescient-retain-classic-highlighting . t))
+      :global-minor-mode t)
 
-        (leaf atom-one-dark-theme
-              :config (load-theme 'atom-one-dark t))
+(leaf atom-one-dark-theme
+      :config (load-theme 'atom-one-dark t))
 
+(leaf nyan-mode
+  :config (nyan-mode)
+          (nyan-start-animation))
 
+(leaf lsp-mode
+  :custom ((lsp-clients-clangd-excutable '/usr/local/opt/llvm/bin//clangd)))
+
+(leaf lsp-ui)
+
+(leaf company-lsp)
+
+(leaf neotree
+  :ensure t)
+  
 (provide 'init)
 
 ;; Splashに画像を表示
@@ -185,6 +198,20 @@
 (defun backward-kill-word-or-kill-region ()
   (interactive)
   (if (or (not transient-mark-mode) (region-active-p))
-      (kill-region (region-beginning) (region-end))
+    (kill-region (region-beginning) (region-end))
     (backward-kill-word 1)))
 (global-set-key "\C-w" 'backward-kill-word-or-kill-region)
+;; default directory
+(setq default-directory "~/")
+(setq command-line-default-directory "~/")
+;; 起動時のサイズ,表示位置,フォントを指定
+(setq initial-frame-alist
+      (append (list
+                '(width . 160)
+                '(height . 55)
+                '(top . 0)
+                '(left . 0)
+                '(font . "Cica-13")
+                )
+              initial-frame-alist))
+
