9264515707823166e280c3f82bfa20e30dd12281
KotaKato <peony.btn@gmail.com>
2021-03-25 01:32:02 +0900

    Update (ひとまずEmacsが使えるようになった)

--- emacs.d/custom.el
+++ emacs.d/custom.el
@@ -4,12 +4,21 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(auto-revert-interval 1)
+ '(company-dabbrev-downcase nil)
+ '(company-idle-delay 0)
+ '(company-minimum-prefix-length 3)
+ '(company-selection-wrap-around t)
+ '(completion-ignore-case t t)
+ '(enable-recursive-minibuffers t)
+ '(ivy-extra-directories nil)
+ '(ivy-height 30)
+ '(ivy-use-virtual-buffers t)
  '(package-archives
    '(("gnu" . "https://elpa.gnu.org/packages/")
      ("melpa" . "https://melpa.org/packages/")
      ("org" . "https://orgmode.org/elpa/")))
  '(package-selected-packages
-   '(leaf-convert use-package nyan-mode neotree leaf-keywords hydra el-get blackout atom-one-dark-theme))
+   '(undo-tree use-package nyan-mode neotree lsp-ui leaf-convert ivy hydra el-get company blackout atom-one-dark-theme))
  '(show-paren-delay 0.1))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
