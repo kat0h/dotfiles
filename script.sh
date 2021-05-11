027859e0af8318562572a2fd279edf3f369440f9
kotakato <peony.btn@gmail.com>
2021-05-12 07:07:59 +0900

    U

---
+++ script.sh
@@ -0,0 +1,14 @@
+cd ~/dotfiles/
+
+while read filename
+do
+		echo "ln -sf `echo $filename | awk '{print "~/dotfiles/" $1 " ~/" $2}'`" | sh
+	done <<END
+tmux.conf .tmux.conf
+vim .vim
+vimrc .vimrc
+vimrc .config/nvim/init.vim
+zshrc .zshrc
+alacritty.yml .alacritty.yml
+emacs.d .emacs.d
+END
