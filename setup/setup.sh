07bdde3ca0640b413f0c245cfbd14b70a6463cfa
Kota Kato <peony.btn@gmail.com>
2022-03-16 23:50:23 +0900

    u

--- setup/setup.sh
+++ setup/setup.sh
@@ -8,6 +8,9 @@ fi
 # install deno
 curl -fsSL https://deno.land/install.sh | sh
 
+export DENO_INSTALL="$deno_install"
+export PATH="$DENO_INSTALL/bin:$PATH"
+
 if [ $? -eq 0 ]; then
   deno -A https://raw.githubusercontent.com/kat0h/dotfiles/master/setup.ts
 fi
