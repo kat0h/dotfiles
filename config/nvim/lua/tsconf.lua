8ae9a5f92baf1b8446dd3a98400551513a22df15
Kota Kato <peony.btn@gmail.com>
2022-11-10 00:43:10 +0900

    Fix config

--- config/nvim/lua/tsconf.lua
+++
@@ -1,7 +0,0 @@
-require('nvim-treesitter.configs').setup {
-  ensure_installed = { "c", "lua", "rust", "vim", "python", "typescript" },
-  auto_install = true,
-  highlight = {
-    enable = true,
-  },
-}
