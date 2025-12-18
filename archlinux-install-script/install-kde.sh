7a7f6a0cd5f5d3169930671e5abd86ca13112df3
Kota Kato <45391880+kat0h@users.noreply.github.com>
2025-12-18 15:21:21 +0900

    -

--- archlinux-install-script/install-kde.sh
+++ archlinux-install-script/install-kde.sh
@@ -5,12 +5,12 @@ set -e -x
 
 sudo pacman -Syyu --noconfirm
 pacman -S --noconfirm plasma dolphin firefox konsole noto-fonts-cjk cups system-config-printer ghostscript power-profiles-daemon kdegraphics-thumbnailers  packagekit-qt6 xdg-desktop-portal xdg-desktop-portal-kde
-sudo systemctl enable --now sddm
 sudo systemctl enable --now cups
 sudo systemctl enable --now bluetooth
 sudo systemctl enable --now power-profiles-daemon
 sudo systemctl disable systemd-networkd systemd-resolved iwd
 sudo systemctl enable NetworkManager
+sudo systemctl enable --now sddm
 
 echo "systemctl start sddm"
 
