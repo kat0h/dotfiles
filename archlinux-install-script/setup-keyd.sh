99c91452ca4323f23cd4fa72a5b4709fe6c6bba4
Kota Kato <45391880+kat0h@users.noreply.github.com>
2026-05-12 14:53:49 +0900

    keydとalacrittyの設定を変更

--- archlinux-install-script/setup-keyd.sh
+++ archlinux-install-script/setup-keyd.sh
@@ -8,7 +8,8 @@ tee /etc/keyd/default.conf <<- KEYD
 *
 
 [main]
-muhenkan = layer(control)
+henkan = overload(control, hangeul)
+muhenkan = overload(control, hanja)
 hangeul = overload(control, hangeul)
 hanja = overload(control, hanja)
 leftcontrol = layer(recontrol)
@@ -23,6 +24,27 @@ h = backspace
 e = end
 a = home
 i = tab
-
 KEYD
+
+tee /etc/keyd/hhkb.conf <<- KEYD2
+[ids]
+04fe:0020
+
+[main]
+henkan = overload(control, hangeul)
+muhenkan = overload(control, hanja)
+rightalt = leftmeta
+leftcontrol = layer(recontrol)
+capslock = layer(recontrol)
+
+[recontrol:C]
+p = up
+n = down
+f = right
+b = left
+h = backspace
+e = end
+a = home
+i = tab
+KEYD2
 
